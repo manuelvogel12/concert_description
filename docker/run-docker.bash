78d616cfdab2441928582f75e4cab0d04a1eb929
Manuel Vogel <ge87xig@mytum.de>
2023-07-13 22:48:33 +0200

    change docker run skript

--- docker/run-docker.bash
+++ docker/run-docker.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-nvidia-docker run --rm -it --gpus all \
+nvidia-docker run -it --gpus all \
  --env="DISPLAY=$DISPLAY" \
  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
  --volume="$HOME/.ssh:/home/user/.ssh:ro" \
  --name concert_description \
- arturolaurenzi/concert_description:latest \
+ manuelvogel12/concert_description:latest \
  x-terminal-emulator
