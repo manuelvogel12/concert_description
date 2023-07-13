0bc1d79ed56c535c35252e2f76de215c60504a64
Manuel Vogel <ge87xig@mytum.de>
2023-07-13 22:50:28 +0200

    change container name to avoid name conflicts

--- docker/run-docker.bash
+++ docker/run-docker.bash
@@ -4,6 +4,6 @@ nvidia-docker run -it --gpus all \
  --env="DISPLAY=$DISPLAY" \
  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
  --volume="$HOME/.ssh:/home/user/.ssh:ro" \
- --name concert_description \
+ --name concert_description_w_sara_shield \
  manuelvogel12/concert_description:latest \
  x-terminal-emulator
