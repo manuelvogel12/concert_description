876a28bd272691d8ac9b172c146bc1ba57f80993
JakobThumm <jakob.thumm@tum.de>
2023-07-19 09:26:05 +0000

    Fixed git ssh error in docker.

--- docker/run-docker.bash
+++ docker/run-docker.bash
@@ -4,6 +4,8 @@ nvidia-docker run -it --gpus all \
  --env="DISPLAY=$DISPLAY" \
  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
  --volume="$HOME/.ssh:/home/user/.ssh:ro" \
+ --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent \
+ --env SSH_AUTH_SOCK=/ssh-agent \
  --name concert_description_w_sara_shield \
  manuelvogel12/concert_description:latest \
  x-terminal-emulator
